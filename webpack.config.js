6962989b327d471e296ed05fd43bee1e76f8ad83
Andy Zhang <andzhng@gmail.com>
2017-12-17 16:22:13 -0800

    Document the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,28 @@ var webpack = require("webpack"),
     path = require("path"),
     fileSystem = require("fs"),
     env = require("./util/env"),
+
+    // Used to remove previous build files before rebuilding new ones
     CleanWebpackPlugin = require("clean-webpack-plugin"),
+
+    // Copies files over from one directory to another
     CopyWebpackPlugin = require("copy-webpack-plugin"),
+
+    // Generates an HTML file. Customizable, and pretty powerful.
     HtmlWebpackPlugin = require("html-webpack-plugin"),
+
+    // Forces webpack-dev-server program to write bundle files to the file system.
+    // TODO: Might not even need this.
     WriteFilePlugin = require("write-file-webpack-plugin");
 
 // load the secrets
 var alias = {};
-
 var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
-
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
-
 if (fileSystem.existsSync(secretsPath)) {
   alias["secrets"] = secretsPath;
 }
+
+var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
 
 var options = {
   entry: {
@@ -24,36 +31,49 @@ var options = {
     options: path.join(__dirname, "chrome-extension", "js", "options.ts"),
     background: path.join(__dirname, "chrome-extension", "js", "background.js")
   },
+
+  // All file outputs from webpack will be under the 'build/' directory.
   output: {
     path: path.join(__dirname, "build"),
     filename: "[name].js"
   },
+
   module: {
     rules: [
       {
+        // Bundles all imported CSS files into one file
         test: /\.css$/,
         loader: "style-loader!css-loader",
         exclude: /node_modules/
       },
       {
+        // Handles loading images
         test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
         loader: "file-loader?name=[name].[ext]",
         exclude: /node_modules/
       },
       {
+        // Handles importing html files in webpack
         test: /\.html$/,
         loader: "html-loader",
         exclude: /node_modules/
       },
       {
+        // Handles compiling TypeScript files into JS
         test: /\.tsx?$/,
         loader: "ts-loader"
       }
     ]
   },
   resolve: {
+    // This is to alias different module names.
+    // For example, we can alias ReactDOM to be 'react-dom' so that we
+    // can import it in a file as
+    // `import * from 'react-dom'`
     alias: alias
   },
+
+  // Plugins that handle a lot of the files moving around.
   plugins: [
     // clean the build folder
     new CleanWebpackPlugin(["build"]),
