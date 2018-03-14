82fab9a27afe887a3e8b5139fdddceb3a42d501f
Andy Zhang <andzhng@gmail.com>
2018-03-14 00:15:32 -0400

    Update analytics.js

--- chrome-extension/js/analytics.js
+++ chrome-extension/js/analytics.js
@@ -1,3 +1,6 @@
+// We collect usage on shortcuts in order to inform future product decisions. 
+// To learn more about what is collected, check out
+// https://github.com/UseMacro/macro-chrome-extension/blob/master/data-policy.md
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
